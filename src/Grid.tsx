799cad9c50ded1ee4f714e30038c1f0a640a9bed
fiorellasaro <fiorellasaro27@gmail.com>
2023-05-31 12:18:44 -0500

    Task #4

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,20 +5,51 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const defaultColDef = {
-  sortable: true
+  sortable: true,
 };
 
+const valueFormatHazartdous = ({ value }: { value: string }) =>
+  value === "Y" ? "Yes" : value === "N" ? "No" : "";
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation", filter: true  },
-  { field: "discovery_date", headerName: "Discovery Date", filter: true, valueFormatter: ({ value }) => new Date(value).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })  },
-  { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter'  },
-  { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter'  },
-  { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter'   },
-  { field: "q_au_2", headerName: "Q (au)", filter: 'agNumberColumnFilter'   },
-  { field: "period_yr", headerName: "Period (yr)", filter: 'agNumberColumnFilter'   },
-  { field: "i_deg", headerName: "Inclination (deg)", filter: 'agNumberColumnFilter'   },
-  { field: "pha", headerName: "Potentially Hazardous", filter: true  },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, filter: true  },
+  { field: "designation", headerName: "Designation", filter: true },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    filter: true,
+    valueFormatter: ({ value }) =>
+      new Date(value).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      }),
+  },
+  { field: "h_mag", headerName: "H (mag)", filter: "agNumberColumnFilter" },
+  { field: "moid_au", headerName: "MOID (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_1", headerName: "q (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_2", headerName: "Q (au)", filter: "agNumberColumnFilter" },
+  {
+    field: "period_yr",
+    headerName: "Period (yr)",
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "pha",
+    headerName: "Potentially Hazardous",
+    filter: true,
+    valueFormatter: valueFormatHazartdous,
+  },
+  {
+    field: "orbit_class",
+    headerName: "Orbit Class",
+    enableRowGroup: true,
+    filter: true,
+  },
 ];
 
 const NeoGrid = (): JSX.Element => {
@@ -27,7 +58,7 @@ const NeoGrid = (): JSX.Element => {
       <AgGridReact
         rowData={data}
         columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
+        rowGroupPanelShow={"always"}
         defaultColDef={defaultColDef}
       />
     </div>
