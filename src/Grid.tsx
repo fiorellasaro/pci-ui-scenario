60e39544e221f66e84a6c7a4749d1a0ebfad33d1
fiorellasaro <fiorellasaro27@gmail.com>
2023-05-31 11:56:20 -0500

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,17 +4,21 @@ import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+const defaultColDef = {
+  sortable: true
+};
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "designation", headerName: "Designation", filter: true  },
+  { field: "discovery_date", headerName: "Discovery Date", filter: true  },
+  { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter'  },
+  { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter'  },
+  { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter'   },
+  { field: "q_au_2", headerName: "Q (au)", filter: 'agNumberColumnFilter'   },
+  { field: "period_yr", headerName: "Period (yr)", filter: 'agNumberColumnFilter'   },
+  { field: "i_deg", headerName: "Inclination (deg)", filter: 'agNumberColumnFilter'   },
+  { field: "pha", headerName: "Potentially Hazardous", filter: true  },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, filter: true  },
 ];
 
 const NeoGrid = (): JSX.Element => {
@@ -24,6 +28,7 @@ const NeoGrid = (): JSX.Element => {
         rowData={data}
         columnDefs={columnDefs}
         rowGroupPanelShow={'always'}
+        defaultColDef={defaultColDef}
       />
     </div>
   );
