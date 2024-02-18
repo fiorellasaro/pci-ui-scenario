d9f8fe1809c7ca4ad033289f9a2a6af93c4691ac
Fiorella Sánchez <fiorella@redi.run>
2024-02-18 13:51:01 -0500

    Task #6

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,5 +1,6 @@
+import { useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
+import { ColDef, GridApi, ColumnApi } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -98,12 +99,38 @@ const columnDefs: ColDef[] = [
 ];
 
 const NeoGrid = (): JSX.Element => {
+  const gridApiRef = useRef<GridApi | null>(null);
+  const columnApiRef = useRef<ColumnApi | null>(null);
+
+  const clearFiltersAndSorting = () => {
+    if (gridApiRef.current && columnApiRef.current) {
+      const allColumns = columnApiRef.current.getAllDisplayedColumns();
+      allColumns.forEach((column) => {
+        column.setSort(undefined);
+      });
+      gridApiRef.current.setFilterModel(null);
+      gridApiRef.current.onFilterChanged();
+    }
+  };
+
+  const onGridReady = (params: { api: GridApi; columnApi: ColumnApi }) => {
+    gridApiRef.current = params.api;
+    columnApiRef.current = params.columnApi;
+  };
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <h1>Near-Earth Object Overview</h1>
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}
+      >
+        <h1 style={{ margin: 0 }}>Near-Earth Object Overview</h1>
+        <button onClick={clearFiltersAndSorting} style={{ marginLeft: "15px" }}>
+          Clear Filters and Sorters
+        </button>
+      </div>
       <AgGridReact
         rowData={data}
         columnDefs={columnDefs}
+        onGridReady={onGridReady}
         rowGroupPanelShow={"always"}
         defaultColDef={{
           sortable: true,
