4e6a5ab81394724d2f4eee349471ab4b736f3107
Fiorella Sánchez <fiorella@redi.run>
2024-02-18 00:49:14 -0500

    Task #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import NeoGrid from './Grid';
+import NeoGrid from "./Grid";
 
 const App = () => {
-  return (
-      <NeoGrid />
-  );
-}
+  return <NeoGrid />;
+};
 
 export default App;
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -14,16 +14,17 @@ const columnDefs: ColDef[] = [
   { field: "period_yr", headerName: "Period (yr)" },
   { field: "i_deg", headerName: "Inclination (deg)" },
   { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+      <h1>Near-Earth Object Overview</h1>
       <AgGridReact
         rowData={data}
         columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
+        rowGroupPanelShow={"always"}
       />
     </div>
   );
